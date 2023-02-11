7c61f6e1303ae89eec855b89e2622852394821bb
AlexBystrov <sshbystrov@gmail.com>
2023-02-11 11:59:18 +0400

    Added answer for exercise 66

---
+++ Exercise66/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая найдет сумму натуральных элементов в промежутке от M до N.
+
+void Main()
+{
+    Console.Write("Enter number M: ");
+    int M = int.Parse(Console.ReadLine()!);
+    Console.Write("Enter number N: ");
+    int N = int.Parse(Console.ReadLine()!);
+    Console.WriteLine(PrintSumNumbers(M, N));
+}
+Main();
+int PrintSumNumbers(int M, int N)
+{
+    if (M == N) return M;
+    else
+        return M + PrintSumNumbers(M + 1, N);
+}
