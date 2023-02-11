eb35dfd4bc275d736a9895da59f084c2c30d529a
AlexBystrov <sshbystrov@gmail.com>
2023-02-11 11:59:00 +0400

    Added answer for exercise 64

---
+++ Exercise64/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+
+string PrintNumbers(int n)
+{
+    if (n == 1) return "1";
+    else 
+    {
+        return $"{n} " + PrintNumbers(n - 1);
+    }
+}
+
+Console.WriteLine(PrintNumbers(8));
